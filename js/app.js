d102a180dd8337a547ef0055b5364f0c1e4bebcd
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2022-11-28 16:42:37 +0400

    working

--- js/app.js
+++ js/app.js
@@ -1,26 +1,59 @@
-function create_tr(table_id) {
-    let table_body = document.getElementById(table_id),
-        first_tr   = table_body.firstElementChild
-        tr_clone   = first_tr.cloneNode(true);
-    table_body.append(tr_clone);
-    clean_first_tr(table_body.firstElementChild);
-}
-function clean_first_tr(firstTr) {
-    let children = firstTr.children;
-    
-    children = Array.isArray(children) ? children : Object.values(children);
-    children.forEach(x=>{
-        if(x !== firstTr.lastElementChild)
-        {
-            x.firstElementChild.value = '';
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    var actions = $("table td:last-child").html();
+    // Append table with add row form on add new button click
+    $(".add-new").click(function () {
+        $(this).attr("disabled", "disabled");
+        var index = $("table tbody tr:last-child").index();
+        var row = '<tr>' +
+            '<td><input type="text" class="form-control" name="Prdouct Name" id="name" placeholder="Ex:Bukhuti Vardanidze"></td>' +
+            '<td><input type="text" class="form-control" name="Prdouct Number" id="pr number" placeholder="Ex: BV-212-VB"></td>' +
+            '<td><input type="text" class="form-control" name="color" id="color" placeholder="Ex: Black"></td>' +
+            '<td><input type="text" class="form-control" name="List Price" id="price" placeholder="Ex: 999 "></td>' +
+            '<td><input type="text" class="form-control" name="date" id="date" ></td>' +
+
+            '<td>' + actions + '</td>' +
+            '</tr>';
+        $("table").append(row);
+        $("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+
+
+
+
+    // Add row on add button click
+    $(document).on("click", ".add", function () {
+        var empty = false;
+        var input = $(this).parents("tr").find('input[type="text"]');
+        input.each(function () {
+            if (!$(this).val()) {
+                $(this).addClass("error");
+                empty = true;
+            } else {
+                $(this).removeClass("error");
+            }
+        });
+        $(this).parents("tr").find(".error").first().focus();
+        if (!empty) {
+            input.each(function () {
+                $(this).parent("td").html($(this).val());
+            });
+            $(this).parents("tr").find(".add, .edit").toggle();
+            $(".add-new").removeAttr("disabled");
         }
     });
-}
-function remove_tr(This) {
-    if(This.closest('tbody').childElementCount == 1)
-    {
-        alert("You Don't have Permission to Delete This ?");
-    }else{
-        This.closest('tr').remove();
-    }
-}
+    // Edit row on edit button click
+    $(document).on("click", ".edit", function () {
+        $(this).parents("tr").find("td:not(:last-child)").each(function () {
+            $(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
+        });
+        $(this).parents("tr").find(".add, .edit").toggle();
+        $(".add-new").attr("disabled", "disabled");
+    });
+    // Delete row on delete button click
+    $(document).on("click", ".delete", function () {
+        $(this).parents("tr").remove();
+        $(".add-new").removeAttr("disabled");
+    });
+});
