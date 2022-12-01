fef34f9684595dca06b2cf6979df017d7d134c0c
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2022-12-02 00:50:11 +0400

    Done

--- js/app.js
+++ js/app.js
@@ -6,11 +6,11 @@ $(document).ready(function () {
         $(this).attr("disabled", "disabled");
         var index = $("table tbody tr:last-child").index();
         var row = '<tr>' +
-            '<td><input type="text" class="form-control" name="Prdouct Name" id="name" placeholder="Ex:Bukhuti Vardanidze"></td>' +
+            '<td><input type="text" class="form-control" name="Prdouct Name" id="name" placeholder=" Ex: HL ... Producr name"></td>' +
             '<td><input type="text" class="form-control" name="Prdouct Number" id="pr number" placeholder="Ex: BV-212-VB"></td>' +
             '<td><input type="text" class="form-control" name="color" id="color" placeholder="Ex: Black"></td>' +
             '<td><input type="text" class="form-control" name="List Price" id="price" placeholder="Ex: 999 "></td>' +
-            '<td><input type="text" class="form-control" name="date" id="date" ></td>' +
+            '<td><input type="text" class="form-control" name="date" id="date" placeholder="Ex: Day/Month/Year" ></td>' +
 
             '<td>' + actions + '</td>' +
             '</tr>';
@@ -28,7 +28,7 @@ $(document).ready(function () {
         var input = $(this).parents("tr").find('input[type="text"]');
         input.each(function () {
             if (!$(this).val()) {
-                $(this).addClass("error");
+                $(this).addClass("error").alert("This field is requared");
                 empty = true;
             } else {
                 $(this).removeClass("error");
@@ -57,3 +57,48 @@ $(document).ready(function () {
         $(".add-new").removeAttr("disabled");
     });
 });
+
+
+
+
+//search
+
+function FunctionNames() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+
+
+// serach hide & show
+var div = document.getElementById("showDiv")
+var display = 1
+var btn = document.getElementById("showFilter")
+
+function showHide() {
+    if (display == 1) {
+        div.style.display = "block"
+        display = 0
+        btn.innerText = "Hide Filter";
+    } else {
+        div.style.display = "none"
+        display = 1
+        btn.innerText = "Show Filter";
+
+    }
+}
+
